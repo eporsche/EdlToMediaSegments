d94a6ef9fa46f38f428ed38ae44cef7cf40d8cde
Rhett Robinson <rrhett@gmail.com>
2025-09-09 23:28:45 -0700

    EdlToMediaSegments: Jellyfin plugin for EDL to MediaSegments conversion

---
+++ EdlToMediaSegmentsProvider.cs
@@ -0,0 +1,90 @@
+using Jellyfin.Database.Implementations.Enums;
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.MediaSegments;
+using MediaBrowser.Controller.Persistence;
+using MediaBrowser.Model;
+using MediaBrowser.Model.MediaSegments;
+using Microsoft.Extensions.Logging;
+
+namespace EdlToMediaSegments;
+
+/// <inheritdoc />
+public class EdlToMediaSegmentsProvider(
+    IItemRepository itemRepository,
+    ILoggerFactory loggerFactory) : IMediaSegmentProvider
+{
+    private readonly ILogger<EdlToMediaSegmentsProvider> logger = loggerFactory.CreateLogger<EdlToMediaSegmentsProvider>();
+
+    /// <inheritdoc />
+    public string Name => "EDL to Media Segments Provider";
+
+    /// <inheritdoc />
+    public ValueTask<bool> Supports(BaseItem item) => new(item is IHasMediaSources);
+
+    /// <inheritdoc />
+    public async Task<IReadOnlyList<MediaSegmentDto>> GetMediaSegments(MediaSegmentGenerationRequest request, CancellationToken cancellationToken)
+    {
+        var item = itemRepository.RetrieveItem(request.ItemId);
+        if (item is not IHasMediaSources mediaItem)
+        {
+            logger.LogDebug("Item {ItemId} is not IHasMediaSources", request.ItemId);
+            return [];
+        }
+
+        // Find EDL file next to the media file.
+        var edlFilePath = Path.ChangeExtension(mediaItem.Path, ".edl");
+        if (!await Task.Run(() => File.Exists(edlFilePath), cancellationToken))
+        {
+            logger.LogDebug("EDL file {EdlFilePath} does not exist for item {ItemId}", edlFilePath, request.ItemId);
+            return [];
+        }
+        logger.LogInformation("Found EDL file {EdlFilePath} for item {ItemId}", edlFilePath, request.ItemId);
+        
+        // Read EDL file and parse the segments.
+        return ParseSegments(await Task.Run(() => File.ReadAllLines(edlFilePath), cancellationToken), item.Id, logger);
+    }
+
+    public static List<MediaSegmentDto> ParseSegments(string[] lines, Guid id, ILogger logger)
+    {
+        var segments = new List<MediaSegmentDto>();
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 3)
+            {
+                logger.LogWarning("EDL line '{Line}' is not in the correct format", line);
+                continue;
+            }
+
+            if (!double.TryParse(parts[0], out var start) || !double.TryParse(parts[1], out var stop) || !int.TryParse(parts[2], out var action))
+            {
+                logger.LogWarning("EDL line '{Line}' has invalid format. Start='{Start}', Stop='{Stop}', Action='{Action}'", line, parts[0], parts[1], parts[2]);
+                continue;
+            }
+            
+            segments.Add(new MediaSegmentDto
+            {
+                Id = Guid.NewGuid(),
+                ItemId = id,
+                Type = action switch
+                {
+                    // This doesn't strictly agree with the EDL spec, but it is what Jellyfin understands.
+                    0 => MediaSegmentType.Intro,
+                    1 => MediaSegmentType.Preview,
+                    2 => MediaSegmentType.Recap,
+                    3 => MediaSegmentType.Commercial,
+                    4 => MediaSegmentType.Outro,
+                    _ => MediaSegmentType.Unknown
+                },
+                StartTicks = ConvertToTicks(start),
+                EndTicks = ConvertToTicks(stop)
+            });
+        }
+
+        return segments;
+    }
+
+    private static long ConvertToTicks(double seconds) => (long)(seconds * TimeSpan.TicksPerSecond);
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+# EDL to Media Segments Plugin
+
+This plugin reads EDL files next to your media files and adds them as Media
+Segments automatically.
+
+This is my solution for https://features.jellyfin.org/posts/180/support-for-edl-comskip-files
+
+## Installation Instructions
+
+1. Add this plugin repository to your server:
+
+In Jellyfin, go to http://localhost:8096/web/#/dashboard/plugins/repositories and add
+
+`https://raw.githubusercontent.com/rrhett/EdlToMediaSegments/refs/heads/main/manifest.json`
+
+2. Install the EdlToMediaSegments plugin from the Metadata section.
+3. Restart Jellyfin.
+
+## How to Use
+
+Next to any media file you wish to generate skips for, add a .edl file with the same
+basename. For example, say you have /path/to/Movie.mp4, then create /path/to/Movie.edl.
+
+The contents of the EDL file are lines following the pattern of:
+
+```
+start stop type
+```
+
+Where `start` is the start time in seconds (e.g. 510.333), `stop` is the stop time in
+seconds, and `type` is the type of the media segment, as follows:
+
+0 - Intro
+1 - Preview
+2 - Recap
+3 - Commercial
+4 - Outro
+
+For example, a file to skip a 1:30 intro and then a commercial break from 4:30.5 to 5:00 would
+look like:
+
+```
+0 90 0
+270.5 300 3
+```
+
+Once you have installed the plugin and added your .edl files, your jellyfin server should
+scan for media segments automatically, but you can force it to do so by going to the
+Dashboard > ScheduledTasks and click the play button next to "Media Segment Scan"
